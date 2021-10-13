385aa548f0ce118027efeec82c4f705b15e4d7c8
Farouk Goaudria <gouadriaafar@gmail.com>
2021-10-13 01:29:34 +0100

    initial commit

---
+++ src/components/Watched.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Watched = () => {
+    return (
+        <div>
+            <h1>watched list</h1>
+        </div>
+    )
+}
---
+++ src/components/Watchlist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Watchlist = () => {
+    return (
+        <div>
+            <h1>
+                watchlist
+            </h1>
+        </div>
+    )
+}
